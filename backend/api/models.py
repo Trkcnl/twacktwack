dd047aae998ffbc4fdcc3f97c4761fd76ae67737
Tarik Canli <tarikcanli10@gmail.com>
2025-12-18 16:06:52 +0100

    add workout, exercise models and serializers

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,55 @@
+from humanize import naturaldelta
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class WorkoutLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="workouts")
+
+    begintime = models.DateTimeField()
+    endtime = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Workout day:{self.begintime.date} - Duration: {self.get_duration()}"
+
+    def get_duration(self):
+        return naturaldelta(self.endtime - self.begintime)
+
+
+class ExerciseType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class ExerciseLog(models.Model):
+    exercise_type = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
+    workout_log = models.ForeignKey(
+        WorkoutLog, on_delete=models.CASCADE, related_name="exercises"
+    )
+
+    def __str__(self):
+        return f"{self.workout_log} - {self.exercise_type}"
+
+
+class ExerciseSet(models.Model):
+    exercise_log = models.ForeignKey(
+        ExerciseLog, on_delete=models.CASCADE, related_name="sets"
+    )
+
+    reps = models.PositiveSmallIntegerField()
+    weight_kg = models.DecimalField(max_digits=6, decimal_places=2)
+    rpe = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)]
+    )
+
+    def __str__(self):
+        return f"{self.reps} reps - {self.weight_kg} kgs"
 
 
 class MeasurementType(models.Model):
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import MeasurementType, Measurement
+from .models import (
+    MeasurementType,
+    Measurement,
+    WorkoutLog,
+    ExerciseType,
+    ExerciseSet,
+    ExerciseLog,
+)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,6 +19,32 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class WorkoutLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkoutLog
+        fields = ["user", "begintime", "endtime", "created"]
+        extra_kwargs = {"user": {"read_only": True}}
+
+
+class ExerciseTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExerciseType
+        fields = ["name", "created"]
+        read_only_fields = fields
+
+
+class ExerciseLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExerciseLog
+        fields = ["exercise_type", "workout_log"]
+
+
+class ExerciseSetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExerciseSet
+        fields = ["exercise_log", "reps", "weight_kg", "rpe"]
 
 
 class MeasurementTypeSerializer(serializers.ModelSerializer):
